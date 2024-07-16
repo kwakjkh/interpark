4fe33bf40d9172a05d0d3eca51c3ac8995f72d15
kwakjkh <1210kwakjkh@gmail.com>
2024-07-16 12:53:37 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -1,32 +1,117 @@
-window.onload = function(){    
-    // 상단의 마이페이지리스트 기능
-    const arrow = document.querySelector(".icon-arrow")
-    const mypage = document.querySelector(".mypage-list")
-    arrow.addEventListener("click",function(){
-        mypage.classList.toggle("show")
-      })
-    
-    // swiper ======================
-    // 프로모션 swiper 기능
-    const swPromotion = new Swiper(".sw-promotion" , {
-        slidesPerView :1 ,
-        spaceBetween: 24,
-        breakpoints: {
-            480: {
-                slidesPerView :2
-            },
-            760 : {
-                slidesPerView :2
-            }
+window.onload = function () {
+  // 상단의 마이페이지리스트 기능
+  const mypageBt = document.querySelector(".mypage-bt");
+  const mypageList = document.querySelector(".mypage-list");
+  mypageBt.addEventListener("click", function () {
+    mypageList.classList.toggle("showMypage");
+  });
+  // swiper ================
+  // 프로모션 스와퍼기능
+  const swPromotion = new Swiper(".sw-promotion", {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    breakpoints: {
+      480: {
+        slidesPerView: 2,
+      },
+      760: {
+        slidesPerView: 2,
+      },
+    },
+    navigation: {
+      nextEl: ".promotion .sw-next",
+      prevEl: ".promotion .sw-prev",
+    },
+    pagination: {
+      el: ".sw-promotion-pg ",
+      clickable: true,
+    },
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
+
+  // 쇼핑의 스와이퍼 기능적용
+  const swShopping = new Swiper(".sw-shopping", {
+    slidesPerView: 1.1,
+    grid: {
+      rows: 1,
+      fill: "row",
+    },
+    spaceBetween: 10,
+    breakpoints: {
+      900: {
+        spaceBetween: 32,
+        slidesPerView: 4,
+        grid: {
+          rows: 1,
         },
-        navigation:{
-            nextEl:".promotion .sw-next",
-            prevEl:".promotion .sw-prev",
+        slidesPerGroup: 3,
+      },
+      1024: {
+        spaceBetween: 32,
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        grid: {
+          rows: 1,
         },
-        loop:true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-          },
-    })
-}
+      },
+      1280: {
+        spaceBetween: 26,
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        grid: {
+          rows: 1,
+        },
+      },
+    },
+    navigation: {
+      nextEl: ".shopping .sw-next",
+      prevEl: ".shopping .sw-prev",
+    },
+    loop:true,
+  });
+
+  // 투어의 스와이퍼 기능적용
+  const swTour = new Swiper(".sw-tour", {
+    // 모바일버전
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    grid: {
+      rows:1,
+      fill: "row",
+    },
+    breakpoints: {
+      480: {
+        spaceBetween: 32,
+        slidesPerView: 3,
+        slidesPerGroup: 2,
+        grid: {
+          rows: 1,
+        },
+      },
+      1024: {
+        spaceBetween: 32,
+        slidesPerView: 3,
+        slidesPerGroup: 2,
+        grid: {
+          rows: 1,
+        },
+      },
+      1280: {
+        spaceBetween: 26,
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        grid: {
+          rows: 1,
+        },
+      },
+    },
+    navigation: {
+      nextEl: ".tour .sw-next",
+      prevEl: ".tour .sw-prev",
+    },
+  })
+};
